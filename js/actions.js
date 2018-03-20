2bb9988382016f469414aa728afa51b5cfca4b50
SistemasCAE <sistemas_cae@cae3076.com>
2018-03-20 17:40:56 -0600

    Pruebas Plugin

--- js/actions.js
+++ js/actions.js
@@ -37,44 +37,44 @@ var fn = {
 	  $("#botonAcceder").tap(fn.iniciarSesion);
 	 
 	 
-	}
-},
-iniciarSesion: function(){
-	cordova.plugins.SitewaertsDocumentViewer.canViewDocument(
-    "http://enlinea.cae3076.com/Portal_CAE/PDFS/2017/AAM9712016M2/20171127/3076-75-7066204/7066204-PC.pdf", 
-	"application/pdf", 
-	options,
-	function onPossible()
-	{
-		entry.canView = true;
-		next();
 	},
-	function onMissingApp()
-	{
-		entry.canView = true;
-		next();
-	},
-	function onImpossible()
-	{
-		entry.canView = false;
-		next();
-	},
-	function onError(error)
-	{
-		window.console.log(error);
-		entry.canView = false;
-		next();
-	});
-};
+	iniciarSesion: function(){
+		console.log("Llegue");
+		cordova.plugins.SitewaertsDocumentViewer.canViewDocument(
+		"http://enlinea.cae3076.com/Portal_CAE/PDFS/2017/AAM9712016M2/20171127/3076-75-7066204/7066204-PC.pdf", 
+		"application/pdf", 
+		options,
+		function onPossible()
+		{
+			entry.canView = true;
+			next();
+		},
+		function onMissingApp()
+		{
+			entry.canView = true;
+			next();
+		},
+		function onImpossible()
+		{
+			entry.canView = false;
+			next();
+		},
+		function onError(error)
+		{
+			window.console.log(error);
+			entry.canView = false;
+			next();
+		});
+	};
+}
 
 
 /*
  *Llamar al metodo Init en el navegador
  */
-//fn.init();
+fn.init();
 
 /*
  *Llamar deviceready para compilar
  */
-//
-fn.deviceready();
+//fn.deviceready();

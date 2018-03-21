87baebf206a80e029e5c860d094680db493dff88
SistemasCAE <sistemas_cae@cae3076.com>
2018-03-21 17:15:27 -0600

    Pruebas Plugin Segundo día

--- js/actions.js
+++ js/actions.js
@@ -10,16 +10,11 @@ var fn = {
 	  $("#botonAcceder").tap(fn.iniciarSesion);
 	},
 	iniciarSesion: function(){
-		//alert("Llegue");
 		var UrlFile = 'http://enlinea.cae3076.com/Portal_CAE/PDFS/2017/AAM9712016M2/20171127/3076-75-7066204/7066204-PC.pdf';
 		var ref = cordova.InAppBrowser.open('https://docs.google.com/viewer?url='+UrlFile+'&embedded=true', '_blank', 'location=yes');
 		window.open = cordova.InAppBrowser.open;
-		
-		//alert("fin metodo");
 	}
 }
-
-
 /*
  *Llamar al metodo Init en el navegador
  */

5466bb8edeae0506736a994727c25a3ecb5ab9cb
SistemasCAE <sistemas_cae@cae3076.com>
2018-03-21 15:10:02 -0600

    Pruebas Plugin Segundo día

--- js/actions.js
+++ js/actions.js
@@ -10,17 +10,40 @@ var fn = {
 	  $("#botonAcceder").tap(fn.iniciarSesion);
 	},
 	iniciarSesion: function(){
+		alert("Llegue");
 		var ref = cordova.InAppBrowser.open('http://enlinea.cae3076.com/Portal_CAE/PDFS/2017/AAM9712016M2/20171127/3076-75-7066204/7066204-PC.pdf', '_blank', 'location=yes');
 		
 		window.open = cordova.InAppBrowser.open;
 		
-		
+		alert("fin metodo");
 		//window.open("http://enlinea.cae3076.com/Portal_CAE/PDFS/2017/AAM9712016M2/20171127/3076-75-7066204/7066204-PC.pdf",'_blank', 'location=no,toolbar=no,hardwareback=yes');
-		
-		/*cordova.plugins.SitewaertsDocumentViewer.viewDocument(
+		/*var _sdv;
+		_sdv = cordova.plugins.SitewaertsDocumentViewer;
+		_sdv.canViewDocument(
 		"http://enlinea.cae3076.com/Portal_CAE/PDFS/2017/AAM9712016M2/20171127/3076-75-7066204/7066204-PC.pdf", 
-		"application/pdf"
-		);
+		"application/pdf", 
+		options,
+		function onPossible()
+		{
+			entry.canView = true;
+			next();
+		},
+		function onMissingApp()
+		{
+			entry.canView = true;
+			next();
+		},
+		function onImpossible()
+		{
+			entry.canView = false;
+			next();
+		},
+		function onError(error)
+		{
+			window.console.log(error);
+			entry.canView = false;
+			next();
+		});*/
 	}
 }
 

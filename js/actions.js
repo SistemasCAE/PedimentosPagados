a78aad06d12e21b2dbd5cf9b53d215d1362a3fa3
SistemasCAE <sistemas_cae@cae3076.com>
2018-03-22 16:44:32 -0600

    Pantalla principal

--- js/actions.js
+++ js/actions.js
@@ -10,9 +10,11 @@ var fn = {
 	  $("#botonAcceder").tap(fn.iniciarSesion);
 	},
 	iniciarSesion: function(){
-		var UrlFile = 'http://enlinea.cae3076.com/Portal_CAE/PDFS/2017/AAM9712016M2/20171127/3076-75-7066204/7066204-PC.pdf';
+		window.location.href="#inicio";
+	
+		/*var UrlFile = 'http://enlinea.cae3076.com/Portal_CAE/PDFS/2017/AAM9712016M2/20171127/3076-75-7066204/7066204-PC.pdf';
 		var ref = cordova.InAppBrowser.open('https://docs.google.com/viewer?url='+UrlFile+'&embedded=true', '_blank', 'location=yes');
-		window.open = cordova.InAppBrowser.open;
+		window.open = cordova.InAppBrowser.open;*/
 	}
 }
 /*

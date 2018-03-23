10933c8d81564f725f7ef3c0f76a909a60b08ff6
SistemasCAE <sistemas_cae@cae3076.com>
2018-03-23 12:30:59 -0600

    Pantalla principal y Aduana en inicio de Sesion

--- js/actions.js
+++ js/actions.js
@@ -11,7 +11,17 @@ var fn = {
 	},
 	iniciarSesion: function(){
 		window.location.href="#inicio";
-	
+		var aduana = $("#aduana").val();
+		if(aduana=='puebla')
+		{
+			aduana = $("#aduanaNp").val('Aduana: Puebla');
+			aduana = $("#aduanaFp").val('Aduana: Puebla');
+		}
+		else
+		{
+			aduana = $("#aduanaNp").val('Aduana: AICM');
+			aduana = $("#aduanaFp").val('Aduana: AICM');
+		}
 		/*var UrlFile = 'http://enlinea.cae3076.com/Portal_CAE/PDFS/2017/AAM9712016M2/20171127/3076-75-7066204/7066204-PC.pdf';
 		var ref = cordova.InAppBrowser.open('https://docs.google.com/viewer?url='+UrlFile+'&embedded=true', '_blank', 'location=yes');
 		window.open = cordova.InAppBrowser.open;*/

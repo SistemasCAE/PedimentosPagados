a0dbb5c7a0c179d3bbdbfc1e8ca00672f2a77c64
SistemasCAE <sistemas_cae@cae3076.com>
2018-03-23 12:58:38 -0600

    Pantalla principal y Aduana en inicio de Sesion

--- js/actions.js
+++ js/actions.js
@@ -14,13 +14,13 @@ var fn = {
 		var aduana = $("#aduana").val();
 		if(aduana=='puebla')
 		{
-			aduana = $("#aduanaNp").val('Aduana: Puebla');
-			aduana = $("#aduanaFp").val('Aduana: Puebla');
+			$("#aduanaNp").text('Aduana: Puebla');
+			$("#aduanaFp").text('Aduana: Puebla');
 		}
 		else
 		{
-			aduana = $("#aduanaNp").val('Aduana: AICM');
-			aduana = $("#aduanaFp").val('Aduana: AICM');
+			$("#aduanaNp").text('Aduana: AICM');
+			$("#aduanaFp").text('Aduana: AICM');
 		}
 		/*var UrlFile = 'http://enlinea.cae3076.com/Portal_CAE/PDFS/2017/AAM9712016M2/20171127/3076-75-7066204/7066204-PC.pdf';
 		var ref = cordova.InAppBrowser.open('https://docs.google.com/viewer?url='+UrlFile+'&embedded=true', '_blank', 'location=yes');
@@ -30,9 +30,9 @@ var fn = {
 /*
  *Llamar al metodo Init en el navegador
  */
-//fn.init();
+fn.init();
 
 /*
  *Llamar deviceready para compilar
  */
-fn.deviceready();
+//fn.deviceready();

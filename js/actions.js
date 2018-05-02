d64bb1b983e2247db333cff3d12073018070d908
SistemasCAE <sistemas_cae@cae3076.com>
2018-05-02 11:48:57 -0500

    Accion al pulsar alerta

--- js/actions.js
+++ js/actions.js
@@ -92,7 +92,7 @@ var fn = {
     	cordova.plugins.notification.badge.set(0);
             navigator.notification.alert(
                 data.message,         // message
-		        fn.accionAlerta(),         // callback
+		        fn.accionAlerta(data.message),         // callback
                 data.title,           // title
                 'Ok'                  // buttonName
             );
